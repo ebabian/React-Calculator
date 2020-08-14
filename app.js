4ae53597945b4f65d7d4cb386bf9e56385b94edf
Elektra Babian <babianelektra@gmail.com>
2020-08-14 14:30:24 -0400

    calc screen shows button value on click

--- app.js
+++ app.js
@@ -2,34 +2,48 @@
 // go to localhost:8000 -> it won't work with index in the browser
 // for css use sass --watch css/main.scss css/main.css in terminal
 class App extends React.Component {
-  
+  state = {
+    val1: '',
+    val2: '',
+    operator: '',
+  }
+
+  displayNum = (event) => {
+    // event.preventDefault();
+    this.setState({
+      val1: event.target.value
+    })
+    console.log(`Val1: ${this.state.val1}`);
+  }
+
+
   render = () => {
     return (   <div className="container">
-        <div id="output" className="screen">0
+        <div id="output" className="screen">{this.state.val1}
         </div>
         <button className="clear" id="clear" name="C">C</button>
 
         <div className="calculator">
 
-            <button className="num">1</button>
-            <button className="num">2</button>
-            <button className="num">3</button>
+            <button onClick={this.displayNum} value="1" className="num">1</button>
+            <button onClick={this.displayNum} value="2" className="num">2</button>
+            <button onClick={this.displayNum} value="3" className="num">3</button>
             <button className="operator">+</button>
 
 
-            <button className="num">4</button>
-            <button className="num">5</button>
-            <button className="num">6</button>
+            <button onClick={this.displayNum} value="4" className="num">4</button>
+            <button onClick={this.displayNum} value="5" className="num">5</button>
+            <button onClick={this.displayNum} value="6" className="num">6</button>
             <button className="operator">-</button>
 
 
-            <button className="num">7</button>
-            <button className="num">8</button>
-            <button className="num">9</button>
+            <button onClick={this.displayNum} value="7" className="num">7</button>
+            <button onClick={this.displayNum} value="8" className="num">8</button>
+            <button onClick={this.displayNum} value="9" className="num">9</button>
             <button className="operator">x</button>
 
 
-            <button className="num">0</button>
+            <button onClick={this.displayNum} value="0" className="num">0</button>
             <button className="num">.</button>
             <button className="equals">=</button>
             <button className="operator">/</button>
