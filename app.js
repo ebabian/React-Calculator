496aa4852c9947e9505cfcd344fd474e93baa78c
Elektra Babian <babianelektra@gmail.com>
2020-08-14 15:53:03 -0400

    add, subtract, multiply, and divide work and the screen displays the answer

--- app.js
+++ app.js
@@ -35,18 +35,49 @@ class App extends React.Component {
     console.log(`Operator: ${this.state.operator}`);
   }
 
+  // calculations
+  calculate = () => {
+    event.preventDefault();
+    if(this.state.operator === '+') {
+      let result = parseInt(this.state.val1) + parseInt(this.state.val2)
+      this.setState({
+        val2: result
+      })
+      console.log(`Result: ${result}`);
+    } else if (this.state.operator === '-') {
+        let result = parseInt(this.state.val1) - parseInt(this.state.val2)
+        this.setState({
+          val2: result
+        })
+        console.log(`Result: ${result}`);
+    } else if (this.state.operator === 'x') {
+        let result = parseInt(this.state.val1) * parseInt(this.state.val2)
+        this.setState({
+          val2: result
+        })
+        console.log(`Result: ${result}`);
+    }  else if (this.state.operator === '/') {
+        let result = parseInt(this.state.val1) / parseInt(this.state.val2)
+        this.setState({
+          val2: result
+        })
+        console.log(`Result: ${result}`);
+      }
+  }
+
   //clear the calc screen when 'C' btn is clicked
   clearScreen = (event) => {
     this.setState({
       val1: '',
-      val2: ''
+      val2: '',
+      operator: ''
     })
     console.log(`cleared!`);
   }
 
   render = () => {
     return (   <div className="container">
-        <div id="output" className="screen">{ this.state.operator === '' ? this.state.val1 : this.state.val2}
+        <div id="output" className="screen">{ this.state.operator === '' ? this.state.val1 : this.state.val2 }
         </div>
         <button onClick={this.clearScreen} className="clear" id="clear" name="C">C</button>
 
@@ -61,19 +92,19 @@ class App extends React.Component {
             <button onClick={this.displayNum} value="4" className="num">4</button>
             <button onClick={this.displayNum} value="5" className="num">5</button>
             <button onClick={this.displayNum} value="6" className="num">6</button>
-            <button className="operator">-</button>
+            <button onClick={this.operator} value="-" className="operator">-</button>
 
 
             <button onClick={this.displayNum} value="7" className="num">7</button>
             <button onClick={this.displayNum} value="8" className="num">8</button>
             <button onClick={this.displayNum} value="9" className="num">9</button>
-            <button className="operator">x</button>
+            <button onClick={this.operator} value="x" className="operator">x</button>
 
 
             <button onClick={this.displayNum} value="0" className="num">0</button>
             <button onClick={this.displayNum} value="." className="num">.</button>
-            <button className="equals">=</button>
-            <button className="operator">/</button>
+            <button onClick={this.calculate} value="=" className="equals">=</button>
+            <button onClick={this.operator} value="/" className="operator">/</button>
 
       </div>
     </div>
