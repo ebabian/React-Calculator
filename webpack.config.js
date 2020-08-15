629637df456781afca59b5b7f57afc663f32a68b
Elektra Babian <babianelektra@gmail.com>
2020-08-15 17:15:10 -0400

    changed the build output from dist to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 entry: './src/index.js',
 output: {
-  path: path.resolve('dist'),
-  filename: 'main.js',
-},
-module: {
-    rules: [
+    path: path.resolve('build'),
+    filename: 'main.js',
+  },
+    devServer: {
+        contentBase: "./build",
+  },
+    module: {
+      rules: [
       { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/}
+      ]
+  },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve('./index.html'),
+      }),
     ]
-  }
 };
